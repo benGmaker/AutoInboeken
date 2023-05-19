1c5b82abe052b6d01a2d5c98635b78b413d7cace
Ben Gortemaker <b.f.gortemaker@student.tue.nl>
2023-05-19 11:10:57 +0200

    Removed virtual environment

--- testFunction.py
+++ testFunction.py
@@ -14,17 +14,17 @@ if __name__ == '__main__':
     if Test_checkNoPop: #Testing the checkNoPop function
         refPopImage = Image.open('TestData/snelstart.png') #load reference
         refPopBuf = np.asarray(refPopImage) #saving reference as array
-        autoinboeken.checkNoPopUp(refPopBuf, "snelstart.png")
-        autoinboeken.checkNoPopUp(refPopBuf, "boekhouden.png" )
-        autoinboeken.checkNoPopUp(refPopBuf, "excelopsherm.png")
-        autoinboeken.checkNoPopUp(refPopBuf, "klantingevuld.png")
-        autoinboeken.checkNoPopUp(refPopBuf, "kleinepopup.png")
-        autoinboeken.checkNoPopUp(refPopBuf, "metprompt.png")
-        autoinboeken.checkNoPopUp(refPopBuf, "filled_in_article.png")
+        autoinboeken.checkNoPopUp(refPopBuf, "TestData/snelstart.png")
+        autoinboeken.checkNoPopUp(refPopBuf, "TestData/boekhouden.png" )
+        autoinboeken.checkNoPopUp(refPopBuf, "TestData/excelopsherm.png")
+        autoinboeken.checkNoPopUp(refPopBuf, "TestData/klantingevuld.png")
+        autoinboeken.checkNoPopUp(refPopBuf, "TestData/kleinepopup.png")
+        autoinboeken.checkNoPopUp(refPopBuf, "TestData/metprompt.png")
+        autoinboeken.checkNoPopUp(refPopBuf, "TestData/filled_in_article.png")
 
     if Test_checkuserisknown:
-        autoinboeken.checkuserisnkown('klantingevuld.png') #screen with user found -> image sum = 91400
-        autoinboeken.checkuserisnkown('snelstart.png') #screen without user found -> image sum = 122400
+        autoinboeken.checkuserisnkown('TestData/klantingevuld.png') #screen with user found -> image sum = 91400
+        autoinboeken.checkuserisnkown('TestData/snelstart.png') #screen without user found -> image sum = 122400
 
     asciArtGenerator.generateASCIart('ASCI-art/66logo.PNG')
     text = ' snelstart inboeken'
